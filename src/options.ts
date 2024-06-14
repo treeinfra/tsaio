90ef49232157db0b2b47fb46b728ddfd44a37c3f
James Aprosail <aprosail@outlook.com>
2024-06-14 08:30:28 +0800

    rollup and ts options

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,13 @@
 import {Config} from "prettier"
+import {RollupOptions} from "rollup"
+import {CompilerOptions} from "typescript"
 
 export type PrettierOptions = Config
 
 export interface Options {
   prettier?: PrettierOptions
+  rollup?: RollupOptions
+  ts?: CompilerOptions
 }
 
 export function defineConfig(options: Options): Options {
