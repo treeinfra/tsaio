6f9a1900a9d7636a82e80f58ce682dbf2af4f324
James Aprosail <aprosail@outlook.com>
2024-06-14 08:34:34 +0800

    vite options

--- src/options.ts
+++ src/options.ts
@@ -1,12 +1,15 @@
-import {Config} from "prettier"
+import {Config as PrettierConfig} from "prettier"
 import {RollupOptions} from "rollup"
 import {CompilerOptions} from "typescript"
+import {UserConfig as ViteUserOptions} from "vite"
 
-export type PrettierOptions = Config
+export type PrettierOptions = PrettierConfig
+export type ViteOptions = ViteUserOptions
 
 export interface Options {
   prettier?: PrettierOptions
   rollup?: RollupOptions
+  vite?: ViteOptions
   ts?: CompilerOptions
 }
 
