89fa91e8762b9d0f048ede2cfe634d9a63850783
James Aprosail <aprosail@outlook.com>
2024-06-14 10:16:02 +0800

    rollup bin file

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import terser from "@rollup/plugin-terser"
 import typescript from "@rollup/plugin-typescript"
+import {readFileSync} from "node:fs"
 import {join} from "node:path"
 import {defineConfig} from "rollup"
 import dts from "rollup-plugin-dts"
@@ -7,11 +8,22 @@ import dts from "rollup-plugin-dts"
 const outDir = "out"
 const srcDir = "src"
 const input = join(srcDir, "index.ts")
-
 const external = ["node:fs", "node:path"]
+const compilerOptions =
+  JSON.parse(readFileSync("tsconfig.json").toString())["compilerOptions"] ?? {}
+
+const buildBin = defineConfig({
+  plugins: [typescript({compilerOptions}), terser()],
+  external,
+  input: join(srcDir, "main.ts"),
+  output: {file: join(outDir, "main.js"), format: "esm"},
+})
 
 const buildLib = defineConfig({
-  plugins: [typescript(), terser()],
+  plugins: [
+    typescript({compilerOptions: {...compilerOptions, sourceMap: true}}),
+    terser(),
+  ],
   input,
   external,
   output: [
@@ -27,4 +39,4 @@ const buildLibDts = defineConfig({
   output: {file: join(outDir, "index.d.ts"), format: "esm"},
 })
 
-export default defineConfig([buildLib, buildLibDts])
+export default defineConfig([buildBin, buildLib, buildLibDts])
