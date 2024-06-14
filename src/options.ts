1ed804b8c1dbfe9ab0b43bf4f4d3fa8233e1f430
James Aprosail <aprosail@outlook.com>
2024-06-14 09:01:16 +0800

    recommend prettier options

--- src/options.ts
+++ src/options.ts
@@ -1,10 +1,16 @@
 import {Config as PrettierConfig} from "prettier"
-import {RollupOptions} from "rollup"
+import {RollupOptions as BaseRollupOptions} from "rollup"
 import {CompilerOptions} from "typescript"
 import {UserConfig as ViteUserOptions} from "vite"
 
 export type PrettierOptions = PrettierConfig
 export type ViteOptions = ViteUserOptions
+export type SingleRollupOptions = BaseRollupOptions
+
+export interface RollupOptions {
+  lib?: SingleRollupOptions
+  bin?: SingleRollupOptions | SingleRollupOptions[]
+}
 
 export interface Options {
   prettier?: PrettierOptions
@@ -13,6 +19,13 @@ export interface Options {
   ts?: CompilerOptions
 }
 
+export const recommendedPrettierOptions: PrettierOptions = {
+  semi: false, // TypeScript prefers no semicolon.
+  bracketSameLine: false, // Save line width space.
+  vueIndentScriptAndStyle: true, // Make vue easier to read.
+}
+
 export function defineConfig(options: Options): Options {
+  options.prettier = options.prettier ?? recommendedPrettierOptions
   return options
 }
