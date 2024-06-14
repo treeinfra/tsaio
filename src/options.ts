b270d1340a288392103a12af3cc5ea93344459ef
James Aprosail <aprosail@outlook.com>
2024-06-14 09:25:14 +0800

    generate rollup options define

--- src/options.ts
+++ src/options.ts
@@ -1,20 +1,16 @@
 import {Config as PrettierConfig} from "prettier"
-import {RollupOptions as BaseRollupOptions} from "rollup"
+import {RollupOptions} from "rollup"
 import {CompilerOptions} from "typescript"
 import {UserConfig as ViteUserOptions} from "vite"
 
+export {RollupOptions} from "rollup"
+
 export type PrettierOptions = PrettierConfig
 export type ViteOptions = ViteUserOptions
-export type SingleRollupOptions = BaseRollupOptions
-
-export interface RollupOptions {
-  lib?: SingleRollupOptions
-  bin?: SingleRollupOptions | SingleRollupOptions[]
-}
 
 export interface Options {
   prettier?: PrettierOptions
-  rollup?: RollupOptions
+  rollup?: RollupOptions | RollupOptions[]
   vite?: ViteOptions
   ts?: CompilerOptions
 }
@@ -23,6 +19,17 @@ export const recommendedPrettierOptions: PrettierOptions = {
   semi: false, // TypeScript prefers no semicolon.
   bracketSameLine: false, // Save line width space.
   vueIndentScriptAndStyle: true, // Make vue easier to read.
+}
+
+export interface RollupBaseOptions {
+  lib?: string | Map<string, string>
+  bin?: string | Map<string, string>
+}
+
+export function rollupOptions(
+  options: RollupBaseOptions,
+): undefined | RollupOptions | RollupOptions[] {
+  return undefined
 }
 
 export function defineConfig(options: Options): Options {
