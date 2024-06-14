f3c97835ab2c8b21fcfea7f92f7848b3cb0e601b
James Aprosail <aprosail@outlook.com>
2024-06-14 09:54:53 +0800

    doc why prettier disable single quote

--- src/templates.ts
+++ src/templates.ts
@@ -1,6 +1,7 @@
 import {Config as PrettierOptions} from "prettier"
 
 export const recommendedPrettierOptions: PrettierOptions = {
+  singleQuote: false, // Compare to the template literals (`xxx`).
   semi: false, // TypeScript prefers no semicolon.
   bracketSameLine: false, // Save line width space.
   vueIndentScriptAndStyle: true, // Make vue easier to read.
